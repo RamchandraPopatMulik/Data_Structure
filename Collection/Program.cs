c91909e46aafbf953a046e888ccf85453aa5164a
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-07 17:37:14 +0530

    Added file

---
+++ Collection/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Collection
+{
+    public class Program
+    { 
+    
+        static void Main(string[] args)
+        {
+            LinkdedList linkdedList = new LinkdedList();
+            linkdedList.Add(56);
+            linkdedList.Add(30);
+            linkdedList.Add(70);
+            linkdedList.Display();
+        }
+    }
+}
