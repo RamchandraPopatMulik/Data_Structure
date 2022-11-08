417c32ecd9c49ad2eb20ce12f36dfbdf7db1e083
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-11-08 11:50:07 +0530

    Added file

---
+++ Collection1/Collection1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Collection1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkdedList = new LinkedList();
+            linkdedList.Add(56);
+            linkdedList.Add(30);
+            linkdedList.Add(70);
+            linkdedList.Display();
+            //linkdedList.InsertAtParticularPosition(2,30);
+            linkdedList.RemoveFirstNode();
+            linkdedList.Display();
+        }
+    }
+}
